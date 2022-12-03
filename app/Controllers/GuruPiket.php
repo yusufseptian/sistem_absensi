16d578b04e2f74edbf57052cf6838a59c8dc3914
yudistjr <wahyudistiragunners@gmail.com>
2022-12-03 23:10:29 +0700

    kelas dinamis

--- app/Controllers/GuruPiket.php
+++ app/Controllers/GuruPiket.php
@@ -6,9 +6,37 @@ use App\Controllers\BaseController;
 
 class GuruPiket extends BaseController
 {
+    private $db = null;
+
+    public function __construct()
+    {
+        $this->db = \Config\Database::connect();
+    }
+
     public function index()
     {
         return redirect()->to(base_url('/'));
+    }
+
+    private function getTANow(): array
+    {
+        $tbTA = $this->db->table('tb_tahun_ajaran');
+        return $tbTA->orderBy('th_id', 'desc')->limit(1)->get()->getResultArray();
+    }
+
+    private function getKelasByGrade(int $grade): array
+    {
+        $tbKelas = $this->db->table('tb_kelas');
+        $dtTA = $this->getTANow();
+        $dtKelas = [];
+        if (count($dtTA) > 0) {
+            $dtTA = $dtTA[0];
+            $dtKelas = $tbKelas->join('tb_tahun_ajaran', 'tahun_ajaran = th_id');
+            $dtKelas->where('tahun_ajaran', $dtTA['th_id']);
+            $dtKelas->where('kelas_grade', $grade);
+            $dtKelas = $dtKelas->get()->getResultArray();
+        }
+        return $dtKelas;
     }
 
     public function kelas10()
@@ -16,6 +44,7 @@ class GuruPiket extends BaseController
         $data = [
             'title' => 'Absensi',
             'subtitle' => 'Kelas 10',
+            'dtKelas' => $this->getKelasByGrade(10)
         ];
         return view('guru_piket/kelas_10', $data);
     }
@@ -25,6 +54,7 @@ class GuruPiket extends BaseController
         $data = [
             'title' => 'Absensi',
             'subtitle' => 'Kelas 11',
+            'dtKelas' => $this->getKelasByGrade(11)
         ];
         return view('guru_piket/kelas_11', $data);
     }
@@ -34,6 +64,7 @@ class GuruPiket extends BaseController
         $data = [
             'title' => 'Absensi',
             'subtitle' => 'Kelas 12',
+            'dtKelas' => $this->getKelasByGrade(12)
         ];
         return view('guru_piket/kelas_12', $data);
     }
--- app/Views/guru_piket/kelas_12.php
+++ app/Views/guru_piket/kelas_12.php
@@ -2,117 +2,21 @@
 <?= $this->section('content') ?>
 
 <div class="row">
-    <div class="col-3">
-        <!-- small box -->
-        <div class="small-box bg-warning">
-            <div class="inner">
-                <h3>IPA A</h3>
-                <h5>Jumlah Data : 12</h5>
+    <?php foreach ($dtKelas as $dt) : ?>
+        <div class="col-3">
+            <!-- small box -->
+            <div class="small-box bg-light">
+                <div class="inner">
+                    <h3><?= $dt['kelas_nama'] ?></h3>
+                    <h5>Jumlah Data : 12</h5>
+                </div>
+                <div class="icon">
+                    <i class="fas fa-user-graduate"></i>
+                </div>
+                <a href="<?= base_url('GuruPiket/Absensi') ?>" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
             </div>
-            <div class="icon">
-                <i class="fas fa-user-graduate"></i>
-            </div>
-            <a href="<?= base_url('jurusan') ?>" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
         </div>
-    </div>
-
-    <div class="col-3">
-        <!-- small box -->
-        <div class="small-box bg-olive">
-            <div class="inner">
-                <h3>IPA B</h3>
-                <h5>Jumlah Data : 12</h5>
-            </div>
-            <div class="icon">
-                <i class="fas fa-book-open"></i>
-            </div>
-            <a href="<?= base_url('pekerjaan') ?>" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
-        </div>
-    </div>
-
-    <div class="col-3">
-        <!-- small box -->
-        <div class="small-box bg-cyan">
-            <div class="inner">
-                <h3>IPA C</h3>
-                <h5>Jumlah Data : 12</h5>
-            </div>
-            <div class="icon">
-                <i class="fas fa-school"></i>
-            </div>
-            <a href="<?= base_url('agama') ?>" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
-        </div>
-    </div>
-
-    <div class="col-3">
-        <!-- small box -->
-        <div class="small-box bg-gray">
-            <div class="inner">
-                <h3>IPA D</h3>
-                <h5>Jumlah Data : 12</h5>
-            </div>
-            <div class="icon">
-                <i class="fas fa-user-tie"></i>
-            </div>
-            <a href="<?= base_url('penghasilan') ?>" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
-        </div>
-    </div>
-
-    <div class="col-3">
-        <!-- small box -->
-        <div class="small-box bg-orange">
-            <div class="inner">
-                <h3>IPS A</h3>
-                <h5>Jumlah Data : 12</h5>
-            </div>
-            <div class="icon">
-                <i class="fas fa-graduation-cap"></i>
-            </div>
-            <a href="<?= base_url('user') ?>" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
-        </div>
-    </div>
-
-    <div class="col-3">
-        <!-- small box -->
-        <div class="small-box bg-green">
-            <div class="inner">
-                <h3>IPS B</h3>
-                <h5>Jumlah Data : 12</h5>
-            </div>
-            <div class="icon">
-                <i class="fas fa-file"></i>
-            </div>
-            <a href="<?= base_url('ppdb') ?>" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
-        </div>
-    </div>
-
-    <div class="col-3">
-        <!-- small box -->
-        <div class="small-box bg-blue">
-            <div class="inner">
-                <h3>IPS C</h3>
-                <h5>Jumlah Data : 12</h5>
-            </div>
-            <div class="icon">
-                <i class="fas fa-file"></i>
-            </div>
-            <a href="<?= base_url('ppdb/listDiterima') ?>" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
-        </div>
-    </div>
-
-    <div class="col-3">
-        <!-- small box -->
-        <div class="small-box bg-dark">
-            <div class="inner">
-                <h3>IPS D</h3>
-                <h5>Jumlah Data : 12</h5>
-            </div>
-            <div class="icon">
-                <i class="fas fa-door-open"></i>
-            </div>
-            <a href="<?= base_url('ppdb/listDitolak') ?>" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
-        </div>
-    </div>
+    <?php endforeach; ?>
 </div>
 
 <?= $this->endSection() ?>
